7cbbd91e196f4eed0f075d3e6b68803f195bd727
Paul Franz <pfranz@franzgames.com>
2020-04-04 14:42:53 -0400

    Make the ircd.sh start script able to run without relying on the current directory being bin

--- res/ircd.sh
+++ res/ircd.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-java -cp ../lib/ircd.jar:../conf org.pastiche.ircd.Ircd
+if [ -z PASTICHE_IRCD_HOME ]; then
+   export PASTICHE_IRCD_HOME=`dirname $0`
+fi
+
+java -cp $PASTICHE_IRCD_HOME/lib/ircd.jar:$PASTICHE_IRCD_HOME/conf org.pastiche.ircd.Ircd
